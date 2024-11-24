47d0b82cc8ea0dc18531444b665a81c3ffd30594
claudiabss <csaleh@usf.edu>
2024-11-24 11:48:54 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
-/**
- * Your JS code here
- */
+// script.js
+let books = [];
 
-/**
- * Sample code
- */
-document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by main.js'));
+function createBook() {
+    const book = {
+        title: document.getElementById('title').value,
+        author: document.getElementById('author').value,
+        genre: document.getElementById('genre').value,
+        description: document.getElementById('description').value,
+    };
+    books.push(book);
+    alert('Book added successfully!');
+    document.getElementById('book-form').reset();
+}
 
-  ulElement.appendChild(liElement);
-});
+function readBooks() {
+    const bookList = document.getElementById('book-list');
+    bookList.innerHTML = '';
+    books.forEach((book, index) => {
+        bookList.innerHTML += `<div>
+            <h3>${book.title}</h3>
+            <p>${book.author} | ${book.genre}</p>
+            <p>${book.description}</p>
+        </div>`;
+    });
+}
+
+function updateBook() {
+    // Mock update functionality
+    alert('Update functionality not implemented in this mock.');
+}
+
+function deleteBook() {
+    // Mock delete functionality
+    alert('Delete functionality not implemented in this mock.');
+}
